382534da347ade1f9d16a243845ca294b4833529
Jan de Jongh <jfcmdejongh@gmail.com>
2020-12-03 20:09:11 +0100

    Added package-info.java.

---
+++ src/main/java/org/javajdj/jswing/jtrace/package-info.java
@@ -0,0 +1,13 @@
+/** A (Swing) package for showing <i>traces</i> or <i>functions</i> the way they are displayed on, e.g., (retro) oscilloscopes
+ *  and spectrum analyzers.
+ * 
+ * <p>
+ * The package supports various combinations of data from traces ({@code double} arrays} and {@code Function}s,
+ * as well as Z-modulation (e.g., though brightness or line thickness).
+ * 
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ * @since 0.7
+ * 
+ */
+package org.javajdj.jswing.jtrace;
