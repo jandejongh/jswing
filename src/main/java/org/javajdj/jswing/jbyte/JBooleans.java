6c23c4d80e49565fc51759999f6392c80f763727
Jan de Jongh <jfcmdejongh@gmail.com>
2022-01-27 15:44:50 +0100

    Fixed (bug) layout for horizontal orientation. Improved use of trim () on label strings. Removed locking (not needed). Full javadoc.

--- src/main/java/org/javajdj/jswing/jbyte/JBooleans.java
+++ src/main/java/org/javajdj/jswing/jbyte/JBooleans.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2020 Jan de Jongh <jfcmdejongh@gmail.com>.
+ * Copyright 2020-2022 Jan de Jongh <jfcmdejongh@gmail.com>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import org.javajdj.jswing.jcolorcheckbox.JColorCheckBox;
 
-/** Swing component for showing a given number of {@code boolean}s as {@link JColorCheckBox.JBoolean}s.
+/** Swing component for showing a given number of labeled {@code boolean}s as {@link JColorCheckBox.JBoolean}s.
  * 
  * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
  * 
@@ -42,6 +42,25 @@ public class JBooleans
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
+  /** Constructs the component.
+   * 
+   * <p>
+   * The label strings are trimmed before use internally, but not in the (displayed) label.
+   * This means you can use spaces in the label strings for (rough) alignment.
+   * 
+   * @param colorFunction         The color function, may be {@code null} or return {@code null},
+   *                                both in which cases the component appears as indicating {@code false},
+   *                                i.e., not filling the square.
+   * @param labelStrings          The label strings, non-{@code null}, non-empty,
+   *                                not containing {@code null}, empty or duplicate strings.
+   * @param horizontalOrientation Whether to outline the booleans horizontally.
+   * 
+   * @throws IllegalArgumentException If the {@code labelStrings} argument is {@code null} or empty,
+   *                                    or contains {@code null} or empty (when trimmed) strings or duplicates.
+   * 
+   * @see JColorCheckBox.JBoolean
+   * 
+   */
   public JBooleans (
     final Function<Boolean, Color> colorFunction,
     final Set<String> labelStrings,
@@ -65,16 +84,56 @@ public class JBooleans
       box.setHorizontalAlignment (SwingConstants.CENTER);
       box.setVerticalAlignment (SwingConstants.CENTER);
       box.setEnabled (false);
-      this.boxMap.put (labelString, box);
-      add (box);      
+      if (this.boxMap.containsKey (labelString.trim ()))
+        throw new IllegalArgumentException ();
+      this.boxMap.put (labelString.trim (), box);
+      add (box);
+      if (! horizontalOrientation)
+      {
+        final JLabel jLabel = new JLabel (labelString);
+        jLabel.setHorizontalAlignment (SwingConstants.LEFT);
+        jLabel.setVerticalAlignment (SwingConstants.CENTER);
+        add (jLabel);
+      }
     }
-    for (final String labelString : labelStrings)
-    {
-      final JLabel jLabel = new JLabel (labelString);
-      jLabel.setHorizontalAlignment (SwingConstants.CENTER);
-      jLabel.setVerticalAlignment (SwingConstants.CENTER);
-      add (jLabel);
-    }
+    if (horizontalOrientation)
+      for (final String labelString : labelStrings)
+      {
+        final JLabel jLabel = new JLabel (labelString);
+        jLabel.setHorizontalAlignment (SwingConstants.CENTER);
+        jLabel.setVerticalAlignment (SwingConstants.CENTER);
+        add (jLabel);
+      }
+  }
+
+  /** Constructs the component.
+   * 
+   * <p>
+   * The label strings are trimmed before use internally, but not in the (displayed) label.
+   * This means you can use spaces in the label strings for (rough) alignment.
+   * 
+   * @param color                The color to use to indicate (filling the square) the {@code true} value,
+   *                               may be {@code null} in which case the component appears as indicating {@code false},
+   *                               i.e., not/never filling the square.
+   * @param labelStrings          The label strings, non-{@code null}, non-empty,
+   *                                not containing {@code null}, empty or duplicate strings.
+   * @param horizontalOrientation Whether to outline the booleans horizontally.
+   * 
+   * @throws IllegalArgumentException If the {@code labelStrings} argument is {@code null} or empty,
+   *                                    or contains {@code null} or empty (when trimmed) strings or duplicates.
+   * 
+   * @see JColorCheckBox.JBoolean
+   * 
+   */
+  public JBooleans (
+    final Color color,
+    final Set<String> labelStrings,
+    final boolean horizontalOrientation)
+  {
+    this (
+      color == null ? null : ((b) -> b != null && b ? color : null),
+      labelStrings,
+      horizontalOrientation);
   }
   
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -91,18 +150,41 @@ public class JBooleans
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  public final synchronized boolean getDisplayedValue (final String key)
+  /** Returns the displayed (boolean) value for given key (label string).
+   * 
+   * <p>
+   * The key argument is trimmed before use.
+   * 
+   * @param key The key.
+   * 
+   * @return The displayed value.
+   * 
+   * @throws IllegalArgumentException If the key is unknown.
+   * 
+   */
+  public final boolean getDisplayedValue (final String key)
   {
-    if (! this.boxMap.containsKey (key))
+    if (! this.boxMap.containsKey (key.trim ()))
       throw new IllegalArgumentException ();
-    return this.boxMap.get (key).getDisplayedValue ();
+    return this.boxMap.get (key.trim ()).getDisplayedValue ();
   }
   
-  public final synchronized void setDisplayedValue (final String key, final boolean displayedValue)
+  /** Sets the displayed (boolean) value for given key (label string).
+   * 
+   * <p>
+   * The key argument is trimmed before use.
+   * 
+   * @param key            The key.
+   * @param displayedValue The value to set.
+   * 
+   * @throws IllegalArgumentException If the key is unknown.
+   * 
+   */
+  public final void setDisplayedValue (final String key, final boolean displayedValue)
   {
-    if (! this.boxMap.containsKey (key))
+    if (! this.boxMap.containsKey (key.trim ()))
       throw new IllegalArgumentException ();
-    this.boxMap.get (key).setDisplayedValue (displayedValue);
+    this.boxMap.get (key.trim ()).setDisplayedValue (displayedValue);
   }
      
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
